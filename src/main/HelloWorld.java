f23542cfa1835838ea419475bf31d1f385fb60b1
galafit <galafit@gmail.com>
2017-02-12 00:01:51 +0100

    Initial commit

---
+++ src/main/HelloWorld.java
@@ -0,0 +1,10 @@
+package main;
+
+/**
+ * Created by gala on 11/02/17.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Display the string.
+    }
+}
